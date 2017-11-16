529a63ce5030e3f7aa919776d2cf2c5fb30a390b
natashaarmbrust <natasha.armbrust@gmail.com>
2017-11-16 01:26:51 -0500

    adding commits over time chart / code

--- contributions.py
+++ contributions.py
@@ -1,4 +1,7 @@
 import mysql.connector
+import csv 
+import matplotlib.pyplot as plt
+import matplotlib
 
 hostname = 'localhost'
 username = '' # no privlidges set so this works
@@ -13,9 +16,36 @@ def queryDB(conn, query) :
     cur.execute(query)
     return cur.fetchall()
 
-result = queryDB(conn, "select language,count(*) from projects where forked_from is null group by language;")
+project_ids = queryDB(conn, "select id from projects where forked_from is null;")
 
-for l, c in result:
-  print l, c
+projects_to_commits =  {} 
+for ID in [x[0] for x in project_ids]:
+  commits = "select project_id, created_at from commits where project_id = {} ORDER BY created_at;".format(ID)
+  rows = queryDB(conn, commits)
+  rows = [r[1] for r in rows] #.strftime('%m/%d/%Y')
+  projects_to_commits[ID] = rows 
 
 conn.close()
+
+fig = plt.figure()
+for ID,datetimes in projects_to_commits.items():
+  dates = matplotlib.dates.date2num(datetimes)
+  plt.plot_date(datetimes,range(1, len(dates) + 1), 'b-')
+
+fig.autofmt_xdate()
+plt.show()
+
+
+#takes in dictionary of project ids to list of creation at dates 
+def create_csv(dictionary):
+  for k,v in projects_to_commits.items():
+    with open(str(k) + '.csv', 'w') as csvfile:
+      fieldnames = ['Commit Creation', 'Total Number of Commits']
+      writer = csv.writer(csvfile,delimiter=" ")
+      writer.writerow(fieldnames)
+      for i,c in enumerate(v):
+        writer.writerow([c,i])
+
+
+# Total number of commits per project 
+#commit_number = "select projects.id, count(*) from projects join commits on projects.id = commits.project_id where projects.forked_from is null group by projects.id;"
