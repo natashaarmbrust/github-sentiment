302e4894f1ec933dbe2399c69fe7418d8592accc
natashaarmbrust <natasha.armbrust@gmail.com>
2017-11-17 04:53:22 -0500

    combined commits + sentiment plots

--- pr_sentiments.py
+++ pr_sentiments.py
@@ -27,6 +27,9 @@ def queryDB(conn, query) :
     cur.execute(query)
     return cur.fetchall()
 
+
+## PR COMMENTS 
+
 project_ids = queryDB(conn, "select projects.id as id, pull_requests.pullreq_id as pullred_id from (select id from projects where forked_from is null) as projects join pull_requests on projects.id = pull_requests.base_repo_id;")[:-1]
 
 projects_to_pr_comments =  {} 
@@ -51,28 +54,51 @@ for ID, PR_ID in project_ids:
       projects_to_pr_comments[ID] = [dates,scores]
   
 
+## COMMITS 
+
+project_ids = queryDB(conn, "select id from projects where forked_from is null;")[:-1]
+
+projects_to_commits =  {} 
+for ID in [x[0] for x in project_ids]:
+  commits = "select project_id, created_at from commits where project_id = {} ORDER BY created_at;".format(ID)
+  rows = queryDB(conn, commits)
+  rows = [r[1] for r in rows] #.strftime('%m/%d/%Y')
+  projects_to_commits[ID] = rows 
 
 conn.close()
 
-plt.figure(figsize=(9, 9)) 
-ax = plt.subplot(111)
-plt.title("Sentiment Analysis Over Time for Pull Requests on the Top 90 Repositories on Github", fontsize=16) 
-plt.ylabel("Sentiment", fontsize=12)
+## PLOT 
+
+
   
 # Ensure that the axis ticks only show up on the bottom and left of the plot.    
 # Ticks on the right and top of the plot are generally unnecessary chartjunk.    
-ax.get_xaxis().tick_bottom()    
-ax.get_yaxis().tick_left()    
+   
 
-i = 0 
+j = 0 
 for ID,date_scores in projects_to_pr_comments.items():
+  fig = plt.figure(figsize=(9, 9)) 
+  ax = plt.subplot(111)
+  plt.title("Total Commits and Sentiment Analysis for Pull Requests Over Time For Repository {} on Github".format(ID), fontsize=10) 
+  #plt.ylabel("Average Sentiment", fontsize=12)
+  ax.get_xaxis().tick_bottom()   
+  ax.get_yaxis().tick_left()
+
   indices = [i[0] for i in sorted(enumerate(date_scores[0]), key=lambda x:x[1])]
   sorted_dates = []
   sorted_scores = []
+  current_sum = 0 
+  count = 0
   for i in indices:
     sorted_dates.append(date_scores[0][i])
-    sorted_scores.append(date_scores[1][i])
-  plt.plot_date(sorted_dates,sorted_scores, '-b')
-  i += 1
+    current_sum += date_scores[1][i]
+    count += 1.0
+    sorted_scores.append(current_sum / count)
+  plt.plot_date(sorted_dates,sorted_scores, '-b', label="Average Sentiment")
 
-plt.show()
+  datetime = projects_to_commits[ID]
+  normalized = [i / float(len(datetime)) for i in range(1, len(datetime) + 1)]
+  plt.plot_date(datetime,normalized, '-g', label="Total Commits (Normalized)")
+  ax.legend(loc=0) 
+  fig.savefig('plots/total_commits_and_average_sentiment/{}_total_commits_and_average_sentiment.png'.format(ID))
+  plt.close(fig) 
